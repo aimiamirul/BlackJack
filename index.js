a046767fc9094aef082260acec1f95e3b42bfbeb
aimi <95113691+aimiamirul@users.noreply.github.com>
2022-03-06 12:25:03 +0800

    update js

--- index.js
+++ index.js
@@ -8,25 +8,40 @@ let startEl = document.getElementById("start-el")
 let playerMoney = document.getElementById("player-money")
 insEl = document.getElementById("ins-el")
 
-//---------------Named items-----------------
+//---------------Nameditems-----------------
 
 let cards = []
 let sum = 0
 let hasBlackJack = false
-let isAlive = false
+let isAlive = true
 let message = ""
-let money = 50
+let money = 250
+let startBtn = true
+let saveMoney = JSON.parse(localStorage.getItem("money"))
 
 //------------------Event----------------
-
+if (localStorage) {
+money = saveMoney
+playerMoney.textContent = `Money : Rm${money}`
+}
 startEl.addEventListener("click", renderGame)
 newEl.addEventListener("click", newCard)
 
 //---------------- Function-----------------
 
 function startGame() {
-    isAlive = true
-    insEl.style.visibility = "hidden"
+    if (money <= 0) {
+        alert("You lost you get your Rm 250 back")
+        money = 250
+        localStorage.setItem("money", JSON.stringify(money))
+        location.reload()
+    } else if (money > 1000) {
+        alert("you are too pro ,reset")
+        money = 250
+        localStorage.setItem("money", JSON.stringify(money))
+        location.reload()
+    }
+    startBtn = false
     hasBlackJack = false
     message = "new card?"
     sumEl.textContent = "sum : " + sum
@@ -38,15 +53,17 @@ function startGame() {
         console.log("new cards")
         //------When you get a blackjack---
     } else if (sum === 21) {
+        startBtn = true
         money += 99
         playerMoney.textContent = "Money: Rm" + money
         hasBlackJack = true
         message = "you have got a BlackJack!"
-        hasBlackJack = true
         // For local database later
         // location.reload()
         //----------When you lose-----------
     } else {
+        startBtn = true
+        isAlive =true
         message = "you are out of game"
         isAlive = false
         money -= 25
@@ -55,6 +72,7 @@ function startGame() {
         // location.reload()
     }
     messageEl.textContent = message
+    localStorage.setItem("money", JSON.stringify(money))
 }
 
 function newCard() {
@@ -67,13 +85,16 @@ function newCard() {
 } 
 
 function renderGame() {
-    isAlive = true
-    let firstCard = getRandomCard()
+    if (startBtn === true){
+        isAlive = true
+        let firstCard = getRandomCard()
     let secondCard = getRandomCard()
     cards = [firstCard, secondCard]
     sum = firstCard + secondCard
     cardEl.textContent = cards
     startGame()
+    }
+    
 }
 
 function getRandomCard() {
@@ -84,5 +105,11 @@ function getRandomCard() {
         return 10
     } else {
         return randomNum
+    }
+}
+
+function fixStartGame() {
+    if (isAlive === true) {
+        startGame()
     }
 }
